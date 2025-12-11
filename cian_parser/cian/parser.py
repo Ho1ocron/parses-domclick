d70c47bcd599281688f9b32f4192ad6a7ef39591
Ho1ocron <cmrfreeze@gmail.com>
2025-12-11 18:27:35 +0500

    added more fields

--- cian_parser/cian/parser.py
+++ cian_parser/cian/parser.py
@@ -1,27 +1,50 @@
 import pandas as pd
+import re
 import io
 
-from cian_parser.constants import COLUMN_MAP, HEADERS
+from cian_parser.constants import COLUMN_MAP
 
 
 class ExcelParser:
-    excel_file: io.BytesIO
+    excel_file: io.BytesIO | str
 
-    def __init__(self, excel_file: io.BytesIO) -> None:
+    def __init__(self, excel_file: io.BytesIO | str) -> None:
         self.excel_file = excel_file
 
-    def _parse_excel(self) -> pd.DataFrame:
+    def _extract_price(self, row: str) -> dict:
+        ifPrepayment = re.search(r"предоплата[:\s]*(\S+)", row, re.IGNORECASE)
+
+        row_slice = row.split(",")
+        price = row_slice[0].split(" ")[0]
+        currency = row_slice[0].split(" ")[1].strip("./") if len(row_slice[0].split(" ")) > 1 else None
+        prepaymnet = row_slice[2] if ifPrepayment else None
+        payment_type = row_slice[0].split(" ")[2] + " " + row_slice[0].split(" ")[3] if len(row_slice) > 1 else None
+        tax = row_slice[-1]
+
+        return {
+            "price": float(price),
+            "currency": currency,
+            "payment_type": payment_type,
+            "prepayment": prepaymnet,
+            "tax": tax,
+        }
+                
+    
+    def clean_excel(self) -> dict:
         dataframe = pd.read_excel(self.excel_file)
         dataframe.iterrows()
         dataframe.columns = dataframe.columns.str.strip()
         dataframe = dataframe.rename(columns=COLUMN_MAP)
         dataframe = dataframe.replace({pd.NA: None})
-
-        return dataframe
-    
-    def _clean_excel(self) -> dict:
-        dataframe = self._parse_excel()
         output_dict = {}
+
+        split_data = dataframe["area"].str.split(",", expand=True)
+
+        dataframe["area"] = split_data[0]
+        dataframe["area_units"] = split_data[1]
+        df_extracted = dataframe["price"].apply(self._extract_price).apply(pd.Series)
+
+        dataframe = pd.concat([dataframe, df_extracted], axis=1)
 
         for _, row in dataframe.iterrows():
             main_key = row.iloc[0]
@@ -31,5 +54,12 @@ class ExcelParser:
         return output_dict
     
 
-    def parse_excel(self) -> dict:
-        return self._clean_excel()
+if __name__ == "__main__":
+    from pathlib import Path
+    import os
+
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+    parser = ExcelParser(str(BASE_DIR / "Downloads/offers.xlsx"))
+    print(parser.clean_excel())
