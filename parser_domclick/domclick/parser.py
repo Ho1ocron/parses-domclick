b0d84cf3095dbef3a5e6fd98e9b97ea354d5ff31
Ho1ocron <cmrfreeze@gmail.com>
2025-12-17 16:56:50 +0500

    [r] floor not showing correctly debug

--- parser_domclick/domclick/parser.py
+++ parser_domclick/domclick/parser.py
@@ -94,10 +94,8 @@ class Parser:
     def _floor(self, item: Dict[str, Any]) -> Optional[str]:
         objectInfo: dict[str, float] = item["objectInfo"]
         floor = objectInfo.get("floor", 10)
-        house: dict[str, float] = item["house"]
-        floors_total = house.get("floorsTotal", 10)
-        if floors_total is not None and floor is not None:
-            return f"{floor}/{floors_total}"
+        if floor is not None:
+            return f"{floor}"
         return None
 
     def _price(self, item: Dict[str, Any]) -> float:
