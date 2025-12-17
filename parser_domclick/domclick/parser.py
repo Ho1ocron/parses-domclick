35c1dd9b4256a764f5102bb6bd06a99a1c659d8e
Ho1ocron <cmrfreeze@gmail.com>
2025-12-17 14:12:05 +0500

    [r] changed limit param to 30 as it is max

--- parser_domclick/domclick/parser.py
+++ parser_domclick/domclick/parser.py
@@ -18,7 +18,7 @@ class Parser:
         for item in items:
             try:
                 offers.append(self._item_to_offer(item))
-                self.logger.debug(f"Appending offers.")
+                self.logger.info(f"Appending offers.")
             except Exception as e:
                 print(f"Error: {e}")
                 self.logger.exception(f"Error during converting offers.", exc_info=True)
